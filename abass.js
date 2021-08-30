ad150b05748963f6e23ca1ae89bd45058250cfc0
abass-adeyemi <78259471+abass-adeyemi@users.noreply.github.com>
2021-08-30 23:41:56 +0100

    Add files via upload

---
+++ abass.js
@@ -0,0 +1,36 @@
+/question 1/;
+function isOldEnoughToDrink(age) {
+	return age >= 18;
+}
+
+/question 2/;
+function isOldEnoughToDrinkAndDrive(age) {
+	return false;
+}
+
+/question 3/;
+function getProperty(object, key) {
+	return object[key];
+}
+
+/ question 4/;
+function addProperty(object, key) {
+	object[key] = true;
+
+	return object.key;
+}
+
+/question 5/;
+function isPersonOldEnoughToDrinkAndDrive(obj) {
+	return false;
+}
+
+/question 6/;
+function computeAverageLengthOfWords(word1, word2) {
+	let length1 = word1.length;
+	let length2 = word2.length;
+
+	let average = (length1 + length2) / 2;
+
+	return average;
+}
